08830325fd2e77d34f75a0ee6ed252fe5fba428b
kitobandito <tijanakordeva@gmail.com>
2026-03-12 17:04:15 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -35,10 +35,4 @@ function squareRoot(a) {
 }
 
 
-// example in browser console by pressing F12
-console.log("Add: " + add(5, 3));
-console.log("Subtract: " + subtract(5, 3));
-console.log("Multiply: " + multiply(5, 3));
-console.log("Divide: " + divide(5, 3));
-console.log("Power: " + power(5, 3));
-console.log("Square Root: " + squareRoot(25));
+
